655b6b9f7dc4b6f6ee0dec07a966af25357c502a
Andrew <aochs1021@gmail.com>
2025-09-16 15:10:27 -0400

    FOnt Sizes Change Accordingly

--- app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
+++ app/src/main/java/edu/temple/inclassuiacvitivity/TextSizeAdapter.kt
@@ -1,25 +1,42 @@
 package edu.temple.inclassuiacvitivity
 
+import android.content.Context
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.TextView
 
-class TextSizeAdapter : BaseAdapter() {
+
+class TextSizeAdapter(private val context: Context, private val textSizes: Array<Int>) : BaseAdapter() {
 
     override fun getCount(): Int {
-        TODO("Not yet implemented")
+        return textSizes.size
     }
 
     override fun getItem(position: Int): Any {
-        TODO("Not yet implemented")
+        return textSizes[position]
     }
 
     override fun getItemId(position: Int): Long {
-        TODO("Not yet implemented")
+        return position.toLong()
     }
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        TODO("Not yet implemented")
+        return if (convertView == null) {
+            TextView(context)
+        } else {
+            convertView as TextView
+        }.apply {
+            text = textSizes[position].toString()
+            textSize = 22f
+        }
+
+    }
+
+    override fun getDropDownView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        return (getView(position, convertView, parent) as TextView).apply {
+            textSize = textSizes[position].toFloat()
+        }
     }
 
 }
